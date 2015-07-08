638975960732e5bf02492397f856e173f9dbd498
Ilya Shalyapin <ishalyapin@gmail.com>
2015-07-08 16:02:30 +0500

    12 tests left

--- urlcan.py
+++ urlcan.py
@@ -9,26 +9,28 @@ from six.moves.urllib.parse import (
 # https://developers.google.com/safe-browsing/developers_guide_v2#Canonicalization
 
 def canonicalize(url):
-    scheme, netloc, path, params, query, fragment = urlparse(url)
-
-    scheme, netloc, path, params, query, fragment = urlparse(url)
-    netloc = netloc.strip('.')
+    scheme, netloc, path, params, query, fragment = urlparse(url.strip())
+    netloc = canonicalize_host(netloc)
     path = canonicalize_path(path)
 
     fragment = ''
-    return urlunparse((scheme, netloc.lower(), path, params, query, fragment))
+    return urlunparse((scheme, netloc, path, params, query, fragment))
 
 
 def canonicalize_path(path):
+    if path == '':
+        return '/'
     prev_path = path
     while True:
         path = unquote(path)
         if path == prev_path:
             break
         prev_path = path
-
     path = os.path.normpath(path)
     path = re.sub('/+', '/', path)
     path = quote(path)
     return path
 
+def canonicalize_host(host):
+    host = host.strip('.')
+    return host.lower()
